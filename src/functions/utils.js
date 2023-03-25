ced8c218e564af24566afde39ff5ceafbd279a40
Hakob.Nersesyan <hakob.nersesian@gmail.com>
2023-03-25 21:13:04 +0100

    update url

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,6 +1,8 @@
 const myUtils = {
   async getData() {
-    const response = await fetch("URL");
+    const response = await fetch(
+      "https://gist.github.com/hakob8956/94212e55b4e07855e226b28bb80e9688/raw"
+    );
     return await response.json();
   },
 };
